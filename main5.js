d0fb75404a0b582ad96848eba2097834e64698ac
cdnuhd <cdnhd@proton.me>
2025-02-06 00:16:56 -0300

    Update and rename main5.js to main6.js

--- main5.js
+++
@@ -1,7 +0,0 @@
-
-injectApp = (() => {
-    try { if(typeof injectApp !== 'undefined') return injectApp; } catch (err) {}
-    return null;
-})();
-injectApp?.success(true);
-parentSuccessScript = true;
